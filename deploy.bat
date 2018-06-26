114fcccd6ee1af8a4446a479ac219b4851ce9a7d
Roberto Albino <junior_wash@hotmail.com>
2018-06-25 23:18:36 -0300

    Deploy Cota Easy Heroku

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,6 @@ git add --all
 git commit -m "Deploy Cota Easy Heroku"
 git push origin master
 git push heroku master
-gradlew build
 heroku deploy:jar --jar build/libs/cota-easy-0.0.1-SNAPSHOT.war --app cota-easy-api
 
 
