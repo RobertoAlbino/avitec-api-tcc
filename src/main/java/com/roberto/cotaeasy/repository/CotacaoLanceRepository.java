60a0a6c12231940b53ddad9f2c9c88596e9cb969
Roberto Albino <junior_wash@hotmail.com>
2018-06-25 23:00:54 -0300

    Adicionado as consultas de lance

--- src/main/java/com/roberto/cotaeasy/repository/CotacaoLanceRepository.java
+++ src/main/java/com/roberto/cotaeasy/repository/CotacaoLanceRepository.java
@@ -4,8 +4,13 @@ import com.roberto.cotaeasy.domain.entities.CotacaoLance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.LinkedList;
+
 @Repository
 public interface CotacaoLanceRepository extends JpaRepository<CotacaoLance, Long> {
     void deleteByCotacaoProdutoId(long produtoId);
     boolean existsByFornecedorEmailAndCotacaoProdutoId(String emailFornecedor, long idProduto);
+    LinkedList<CotacaoLance> getAllByCotacaoId(long idCotacao);
+    LinkedList<CotacaoLance> getAllByCotacaoUsuarioId(long idUsuario);
+    LinkedList<CotacaoLance> getAllByCotacaoUsuarioIdAndCotacaoProdutoId(long idUsuario, long idProduto);
 }
