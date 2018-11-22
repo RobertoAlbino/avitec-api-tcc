333117f14f1f66474f24061511e98482598525c3
Roberto Albino Junior <junior_wash@hotmail.com>
2018-11-22 19:40:28 -0200

    Deploying avitec

--- src/main/java/com/roberto/avitec/repository/LogEmailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.roberto.avitec.repository;
-
-import com.roberto.avitec.domain.entities.LogEmail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogEmailRepository extends JpaRepository<LogEmail, Long> {}
