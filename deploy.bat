c1bb8bdfabff2190a84a1640f990db8bcc215273
Roberto Albino Junior <junior_wash@hotmail.com>
2018-11-28 12:45:13 -0200

    Deploying avitec

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,8 @@ git add --all
 git commit -m "Deploying avitec"
 git push avi master
 git push heroku master
-heroku deploy:jar --jar build/libs/avitec-0.0.1-SNAPSHOT.war --app avitec-api
+gradle build
+heroku heroku deploy:jar --jar build/libs/avitec-0.0.1-SNAPSHOT.war --app avitec-api
 
 
 
